3947e2e82990657d98b7582f2953d6a2f06531be
Matheus <matheusrsilva@gmail.com>
2021-08-20 12:49:51 -0300

    password update is possible

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -4,25 +4,26 @@ const { database, fetch, update, getValidToken } = require('../config/database')
 const { generateToken, validateEmail, cryptPassword, checkPassword } = require('../utils');
 
 exports.authorize = async (method, params, headers) => {
-  switch(method){
-    case 'GET':{
-      if (!params.id) return true; 
+  switch (method) {
+    case 'GET': {
+      if (!params.id) return true;
     }
-    case 'PUT':{
+    case 'PUT': {
       const token = await getValidToken(headers.authorization)
       if (token instanceof Error) return token;
       // user by token is the same who is updated
       if (token && token.user_id === parseInt(params.id, 10)) return true;
       return false;
     }
-      default: return true;
+    default: return true;
   }
 }
 
-const read = async (id) => {
+const read = async (id, keep_password = false) => {
   const [user] = await fetch('user', { id });
-  delete user.password;
-  return user; 
+  if (!keep_password)
+    delete user.password;
+  return user;
 }
 exports.read = read;
 
@@ -32,7 +33,7 @@ exports.readByToken = async (token) => {
 
   const [user] = await fetch('user', { id: foundToken.user_id });
   delete user.password;
-  return user; 
+  return user;
 }
 
 exports.create = async (body) => {
@@ -43,15 +44,15 @@ exports.create = async (body) => {
     date_of_birth
   } = body;
 
-  if(!(username && validateEmail(username))) return new Error('Invalid Username, must be a valid email');
-  const [oldUser] = await fetch('user', {username});
-  if(oldUser) return new Error('Email is in use already');
-  if(!password) return new Error('Password is Required');
-  if(!fullname) return new Error('Fullname is Required');
+  if (!(username && validateEmail(username))) return new Error('Invalid Username, must be a valid email');
+  const [oldUser] = await fetch('user', { username });
+  if (oldUser) return new Error('Email is in use already');
+  if (!password) return new Error('Password is Required');
+  if (!fullname) return new Error('Fullname is Required');
 
   const cryptoPass = await cryptPassword(password, (res => {
     console.log(res);
-  })); 
+  }));
   console.log(cryptoPass);
 
   const [user_id] = (await database('user').insert({
@@ -67,13 +68,21 @@ exports.create = async (body) => {
 exports.update = async (id, body) => {
   const {
     fullname,
-    date_of_birth
+    date_of_birth,
+    password,
+    old_password
   } = body;
 
   const fields = {};
   if (fullname) fields.fullname = fullname;
   if (date_of_birth) fields.date_of_birth = date_of_birth;
-
+  if (password) {
+    if (!old_password) return new Error("Need old_password to set a new password");
+    const user = await read(id, true);
+    const isValidPassword = await checkPassword(old_password, user.password);
+    if (!isValidPassword) return new Error("old_password doesn't match");
+    fields.password = await cryptPassword(password);
+  }
   await update('user', fields, id);
   return read(id);
 };
@@ -84,14 +93,14 @@ exports.login = async (body) => {
     password,
   } = body;
 
-  const [user] = await fetch('user', { username});
+  const [user] = await fetch('user', { username });
   if (!user) return new Error("User is not registered");
 
   const isValidPassword = await checkPassword(password, user.password);
   if (!isValidPassword) return new Error("User/Password doesn't match");
 
   const [token] = await fetch('token', { user_id: user.id })
-  
+
   if (token) {
     if (moment(token.expiration, 'YYYY-MM-DD HH:mm:ss').isBefore(moment())) {
       const newToken = generateToken();
